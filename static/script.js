077649cf3b60752d0703c3eda3d60b5b6253c863
martaw-code <marta.granero.i@estudiantat.upc.edu>
2025-01-16 00:54:09 +0100

    casi finish

---
+++ static/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
